2420e2eaf9049c4343735bc9c982227b1436b583
dhh <david@loudthinking.com>
2024-02-14 16:00:54 -0800

    Excess CR

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     resources :leafs
   end
 
-
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
