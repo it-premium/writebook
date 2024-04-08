54632234f788f229d25fa3170573cb78ee44f0fc
Kevin McConnell <kevin@37signals.com>
2024-04-08 14:24:41 +0100

    Make it easier to get to the trashed items

--- app/models/leaf/statuses.rb
+++ app/models/leaf/statuses.rb
@@ -5,5 +5,7 @@ module Leaf::Statuses
     enum :status, %w[ draft published trashed ].index_by(&:itself), default: :draft
 
     default_scope { where.not(status: :trashed) }
+
+    scope :including_trashed, -> { unscope(where: :status) }
   end
 end
---
+++ test/models/leaf/statuses_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class Leaf::StatusesTest < ActiveSupport::TestCase
+  test "trashed items are excluded by default" do
+    assert_changes -> { books(:handbook).leaves.count }, from: 4, to: 3 do
+      leaves(:summary_page).trashed!
+    end
+  end
+
+  test "trashed items still exist" do
+    assert_no_changes -> { books(:handbook).leaves.including_trashed.count } do
+      leaves(:summary_page).trashed!
+    end
+  end
+end
