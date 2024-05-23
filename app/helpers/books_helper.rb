55a29adad2a06983a30fc3cbc10db70712e15d95
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-05-23 18:26:35 -0500

    Navigation experiments
    - Keyboard arrow navigation now loops through book content starting and ending with the TOC page
    - Sections now color the entire page
    - Lower nav book navbar is static so you don't see it until you reach the bottom of the page
    - Only the next page button is visible (but you can go back with the keyboard shortcut)

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -12,22 +12,46 @@ module BooksHelper
       }, &
   end
 
+  def link_to_first_leafable(leaves)
+    if first_leaf = leaves.first
+      link_to leafable_path(first_leaf), data: { **hotkey_data_attributes("right") }, class: "txt-ink txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-start flex-item-justify-start", hidden: true do
+        tag.span(class: "btn") do
+          image_tag("arrow-right.svg", aria: { hidden: true }, size: 24) + tag.span("Start reading", class: "for-screen-reader")
+        end + tag.span(first_leaf.title, class: "overflow-ellipsis")
+      end
+    end
+  end
+
   def link_to_previous_leafable(leaf)
     if previous_leaf = leaf.previous
-      link_to leafable_path(previous_leaf), data: { **hotkey_data_attributes("left") }, class: "txt-ink txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-start flex-item-justify-start" do
+      link_to leafable_path(previous_leaf), data: { **hotkey_data_attributes("left") }, class: "txt-ink txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-start flex-item-justify-start", hidden: true do
         tag.span(class: "btn") do
           image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Previous", class: "for-screen-reader")
         end + tag.span(previous_leaf.title, class: "overflow-ellipsis")
+      end
+    else
+      link_to book_path(leaf.book), data: { **hotkey_data_attributes("left") }, class: "txt-ink txt-medium txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-end flex-item-justify-end", hidden: true do
+        tag.span("Beginning", class: "overflow-ellipsis") +
+        tag.span(class: "btn txt-medium") do
+          image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Beginning", class: "for-screen-reader")
+        end
       end
     end
   end
 
   def link_to_next_leafable(leaf)
     if next_leaf = leaf.next
-      link_to leafable_path(next_leaf), data: { **hotkey_data_attributes("right") }, class: "txt-ink txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-end flex-item-justify-end" do
+      link_to leafable_path(next_leaf), data: { **hotkey_data_attributes("right") }, class: "txt-ink txt-medium txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-end flex-item-justify-end" do
         tag.span(next_leaf.title, class: "overflow-ellipsis") +
-        tag.span(class: "btn") do
+        tag.span(class: "btn txt-medium") do
           image_tag("arrow-right.svg", aria: { hidden: true }, size: 24) + tag.span("Next", class: "for-screen-reader")
+        end
+      end
+    else
+      link_to book_path(leaf.book), data: { **hotkey_data_attributes("right") }, class: "txt-ink txt-medium txt-undecorated flex align-center gap full-width flex-item-grow min-width justify-end flex-item-justify-end" do
+        tag.span("End", class: "overflow-ellipsis") +
+        tag.span(class: "btn txt-medium") do
+          image_tag("arrow-right.svg", aria: { hidden: true }, size: 24) + tag.span("End", class: "for-screen-reader")
         end
       end
     end
