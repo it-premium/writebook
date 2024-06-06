8f8209938520ab130c041a3923a450fe754c7fb7
Kevin McConnell <kevin@37signals.com>
2024-06-06 09:25:49 +0100

    Move to a concern

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  include Accesses, Sluggable
+  include Accesses, DemoContent, Sluggable
 
   has_many :leaves, dependent: :destroy
   has_one_attached :cover
@@ -7,22 +7,10 @@ class Book < ApplicationRecord
   scope :ordered, -> { order(:title) }
   scope :published, -> { where(published: true) }
 
-  after_create :create_demo_leaves
-
   def press(leafable, leaf_params)
     transaction do
       leafable.save!
       leaves.create! leaf_params.merge(leafable: leafable)
     end
-  end
-
-  def create_demo_leaves
-    image = File.open(Rails.root.join("app/assets/images/demo/inspiration-is-perishable.png"))
-    picture = Picture.new(caption: "Inspiration is perishable")
-    picture.image.attach(io: image, filename: "inspiration-is-perishable.png", content_type: "image/png")
-
-    press Section.new(), title: "Chapter 1"
-    press Page.new(body: "# Welcome to Writebook. \n\n Your new book begins here."), title: "My first page"
-    press picture, title: "Figure 1"
   end
 end
