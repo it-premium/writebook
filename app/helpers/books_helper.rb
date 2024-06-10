f3a03de58889d7cf008a2b1b5097d7df974b561a
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-06-10 14:50:34 -0500

    Better accessible labels

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -36,11 +36,11 @@ module BooksHelper
     if previous_leaf = leaf.previous
       path = for_edit ? edit_leafable_path(previous_leaf) : leafable_path(previous_leaf)
       link_to path, data: hotkey_data_attributes("left", enabled: hotkey), class: "btn flex-item-justify-start" do
-        image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span(previous_leaf.title, class: "for-screen-reader")
+        image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Previous: #{ previous_leaf.title }", class: "for-screen-reader")
       end
     else
       link_to Current.user ? book_path(leaf.book) : public_book_path(leaf.book.slug), data: hotkey_data_attributes("left", enabled: hotkey), class: "btn flex-item-justify-start" do
-        image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Table of contents", class: "for-screen-reader")
+        image_tag("arrow-left.svg", aria: { hidden: true }, size: 24) + tag.span("Table of contents: #{ leaf.book.title }", class: "for-screen-reader")
       end
     end
   end
@@ -49,11 +49,11 @@ module BooksHelper
     if next_leaf = leaf.next
       path = for_edit ? edit_leafable_path(next_leaf) : leafable_path(next_leaf)
       link_to path, data: hotkey_data_attributes("right", enabled: hotkey), class: "btn txt-medium min-width" do
-        tag.span(next_leaf.title, class: "overflow-ellipsis") + image_tag("arrow-right.svg", aria: { hidden: true }, size: 24)
+        tag.span("Next: #{next_leaf.title }", class: "overflow-ellipsis") + image_tag("arrow-right.svg", aria: { hidden: true }, size: 24)
       end
     else
       link_to Current.user ? book_path(leaf.book) : public_book_path(leaf.book.slug), data: hotkey_data_attributes("right", enabled: hotkey), class: "btn txt-medium" do
-        tag.span(leaf.book.title, class: "overflow-ellipsis") + image_tag("arrow-reverse.svg", aria: { hidden: true }, size: 24)
+        tag.span("Table of contents: #{leaf.book.title }", class: "overflow-ellipsis") + image_tag("arrow-reverse.svg", aria: { hidden: true }, size: 24)
       end
     end
   end
