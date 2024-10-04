e97506b9cffd4abcabde469c59f742d49f56f2fe
Kevin McConnell <kevin@37signals.com>
2024-10-04 14:28:42 +0100

    Add helper for sanitized markdown with highlights

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,7 +8,11 @@ module PagesHelper
     [ leaf.title, book.title, book.author ].reject(&:blank?).to_sentence(two_words_connector: " · ", words_connector: " · ", last_word_connector: " · ")
   end
 
-  def sanitize_content(content)
-    sanitize content, scrubber: HtmlScrubber.new
+  def highlighted_match(match)
+    sanitize match, tags: %w[ mark ]
+  end
+
+  def sanitize_content(content, **)
+    sanitize content, scrubber: HtmlScrubber.new, **
   end
 end
